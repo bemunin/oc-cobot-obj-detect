589e0aa26e0ebbaa1240d74deab56ab555d427a4
bemunin <tw.munin@gmail.com>
2025-01-26 12:05:41 +0700

    Add Franka to simple env sim

--- sim/oc.scene.cobot_simple/oc/scene/cobot_simple/simple_env_sim.py
+++ sim/oc.scene.cobot_simple/oc/scene/cobot_simple/simple_env_sim.py
@@ -1,6 +1,8 @@
+import numpy as np
 from oc.utils.cobot import BaseSim
 from oc.utils.cobot.robots.franka_manager import FrankaManager
 from omni.isaac.core.scenes import Scene
+from omni.isaac.menu import set_camera_view
 
 from .tasks import EnvCreator
 
@@ -10,6 +12,10 @@ class SimpleEnvSim(BaseSim):
         super().__init__()
 
     def set_up_scene(self, scene: Scene):
+        set_camera_view(
+            eye=np.array([2.55, 2.45, 2.65]),
+            target=np.zeros(3),
+        )
+
         self._world.add_task(EnvCreator(name="env_task"))
         self._world.add_task(FrankaManager(name="franka_task"))
-        return
--- sim/oc.utils.cobot/oc/utils/cobot/robots/franka_manager.py
+++ sim/oc.utils.cobot/oc/utils/cobot/robots/franka_manager.py
@@ -1,14 +1,23 @@
 from omni.isaac.core.scenes import Scene
+from omni.isaac.franka import Franka
+from omni.isaac.franka.controllers import PickPlaceController
 
 from ..bases.creator_task import CreatorTask
 
 
 class FrankaManager(CreatorTask):
     def set_up_scene(self, scene: Scene):
-        pass
+        super().set_up_scene(scene)
+        self._franka = Franka(prim_path="/World/Franka", name="franka")
+
+        scene.add(self._franka)
 
     def post_reset(self):
-        pass
+        self._controller = PickPlaceController(
+            name="pick_place_controller",
+            gripper=self._franka.gripper,
+            robot_articulation=self._franka,
+        )
 
     def pre_step(self, time_step_index: int, simulation_time: float):
         pass
